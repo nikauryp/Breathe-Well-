dced5cdfce304be14244819a65c53cf5bc57776e
jacqueline rodriguez <rodriguezj@brandeis.edu>
2018-11-19 11:43:27 -0500

    Update asthma.py

--- asthma.py
+++ asthma.py
@@ -1,12 +1,21 @@
+#  ^X^S is to save ^X^C is to quit use arrow keys and delete key to edit
+# ^A begin line ^E end line  ^PNFB  ^K kill rest of line ^Y yanks it back
 print("Asthma Crisis Helper")
 
 # ask questions and then give answers to user
 def helper():
-    danger = input("Can the person breathe at all? (yes or no)")
-    if (danger == no):
+    reply = input("Can the person breathe at all? (yes or no)")
+    if (reply == 'no'):
         print("Call 911 right away!")
+        return
+    reply = input("Do they have an inhaler? (yes or no)")
+    if (reply=='yes'):
+        print("Have them use the inhaler")
     else:
-        notcritical()
+        print("sit up right and try to take deep breaths")
 
 def noncritical():
   print("call your doctor")
+
+def emergencyvisit():
+    tobring= input("what to bring to hospital")
