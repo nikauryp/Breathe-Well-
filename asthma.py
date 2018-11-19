c4552613f81d771923b2638889baf5a95e97017b
jacqueline rodriguez <rodriguezj@brandeis.edu>
2018-11-19 11:23:13 -0500

    first commit

---
+++ asthma.py
@@ -0,0 +1,12 @@
+print("Asthma Crisis Helper")
+
+# ask questions and then give answers to user
+def helper():
+    danger = input("Can the person breathe at all? (yes or no)")
+    if (danger == no):
+        print("Call 911 right away!")
+    else:
+        notcritical()
+
+def noncritical():
+  print("call your doctor")
